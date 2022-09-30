c4f2689a74ac6a469d3f2b6986c87b0aa964bf7a
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-30 12:08:19 +0900

    Hello World出力

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller //アノテーション Springにそのクラスがコントローラーであることを伝えるためのもの。
 public class PostController {
-    @GetMapping("/hello")
-    @ResponseBody
+    @GetMapping("/hello") //アノテーション　railsのルーティング的な役割。ルートパス/helloというパスに行ったら下記メソッドを実行する。GetはHTTPメソッドのこと、そのためPostMappingもある。
+    @ResponseBody //アノテーション　リクエストに対して、HTMLを返すためのもの。下記メソッドでreturnされる文字列を、HTMLと解釈させるためのもの。
     public String showHello(){
         return "<h1>Hello World!</h1>";
     }
