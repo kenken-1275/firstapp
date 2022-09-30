4e9622963e498f2967fbe3ce873e232024a82d83
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-30 15:37:03 +0900

    コントローラーでオブジェクト作成

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller //アノテーション Springにそのクラスがコントローラーであることを伝えるためのもの。
 public class PostController {
     @GetMapping("/hello") //アノテーション　railsのルーティング的な役割。ルートパス/helloというパスに行ったら下記メソッドを実行する。GetはHTTPメソッドのこと、そのためPostMappingもある。
@@ -11,5 +13,15 @@ public class PostController {
         var sampleText = "サンプルテキスト"; //変数を定義
         model.addAttribute("sampleText", sampleText); //addAttributeでmodelに要素を追加している。1つ目の引数がビューで読み込む際の名称、2つ目の引数が直前の変数を表している。
         return "hello"; //この記述でresourcesフォルダのtemplatesフォルダに作成したhello.htmlを読み込める。
+    }
+    @GetMapping  //@GetMapping("/")と同じ意味。ルートパスの時は省略できる。
+    public String showList(Model model){
+        var postList = List.of( //この記述で、3件のデータを作成している。List.ofは文字通りリストを作成するメソッド。
+                new PostEntity(1,"投稿1"), //PostEntityクラスのオブジェクトを3つ作成している。
+                new PostEntity(1,"投稿2"),
+                new PostEntity(1,"投稿3")
+        );
+        model.addAttribute("postList",postList);//作成したオブジェクトをmodelに追加している。
+        return "index";
     }
 }
