d038afb93ec88dd737f4c776f2f0bbc6d6dd98bc
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-10-01 13:43:29 +0900

    DBのテーブル作成とカラムの設定。

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,8 @@
+--gradleファイルにDB作成用のライブラリ導入後に、schema.sqlを作成し下記内容の記述を行う。
+--postsテーブルを作成している。
 CREATE TABLE IF NOT EXISTS posts (
+--idカラムの作成(BIGINTが型、NOTNULL制約と、主キー設定、AUTO_INCREMENTは自動で数字を上げていってくれる設定)
     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--memoカラムの作成(VARCHAR型、NOTNULL制約設定。)
     memo VARCHAR(256) NOT NULL
 );
