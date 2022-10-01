78b51168cb2ed84e0ecd4f841aaca43ae29904a3
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-10-01 15:40:52 +0900

    リポジトリにinsertメソッド定義

--- src/main/java/in/techcamp/firstapp/PostRepository.java
+++ src/main/java/in/techcamp/firstapp/PostRepository.java
@@ -3,6 +3,7 @@
 
 package in.techcamp.firstapp;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,4 +13,7 @@ import java.util.List;
 public interface PostRepository {
     @Select("select * from posts") //@Select("行いたいSQL文")
     List<PostEntity> findAll(); //返り値のデータ型　メソッド名;　と記述する。
+
+    @Insert("insert into posts (memo) values (#{memo})") //引数で渡された変数memoをpostsテーブルのmemoカラムに保存するよう設定している。
+    void insert(String memo);
 }
